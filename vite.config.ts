ce8bb2fe0cc4fdf4c3b26e6e87a6104b8783f6a9
chibx <111882496+chibx@users.noreply.github.com>
2026-02-23 21:40:37 +0100

    Added the hono plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import devServer from "@hono/vite-dev-server";
+import { build } from "@hono/vite-build/node";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    devServer({
+      entry: "backend/server.ts",
+    }),
+    build({
+      // Defaults are `src/index.ts`,`./src/index.tsx`,`./app/server.ts`
+      entry: "./src/index.tsx",
+      // port option is only for Node.js adapter. Default is 3000
+      port: 3001,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
